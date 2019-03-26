e01c00ad9728324725d65ac4cd6458720371d694
cslove <969118892@qq.com>
2019-03-26 18:39:26 +0800

    module init

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-(function(name, defination) {
-    // const hasDefine = typeof define === function ?
+(function (name, defination) {
+    /** 
+     * Determine the environment and export the interface
+     * (amd/cmd commonjs windows)
+     */
+  const hasDefine = typeof define === 'function'
+  const hasModule = typeof module === 'object'
+  if (hasDefine) {
+    define(defination)
+  } else if (hasModule) {
+    module.exports = defination()
+  } else {
+    this[name] = defination()
+  }
 
-})('isReg', function() {
+})('isReg', function () {
 
 })
