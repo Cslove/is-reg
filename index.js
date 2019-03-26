5b4481be3b9985919c348be9d9be1ed9c1e5782e
cslove <969118892@qq.com>
2019-03-26 19:48:31 +0800

    add phone password

--- index.js
+++ index.js
@@ -17,7 +17,25 @@
     const reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
     return reg.test(str)
   }
+
+  const isPhone = function(str) {
+    const reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+    return reg.test(str)
+  }
+
+  const isPassword = function(str) {
+    const reg = /^[a-zA-Z]\w{5,17}$/
+    return reg.test(str)
+  }
+
+  const isStrongPassword = function(str) {
+    const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$/
+    return reg.test(str)
+  }
   return {
     isEmail,
+    isPhone,
+    isPassword,
+    isStrongPassword,
   }
 })
